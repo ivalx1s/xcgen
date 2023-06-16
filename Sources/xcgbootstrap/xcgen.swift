7d6d884bce75e71d70adfb5f38196ad22c3e9e48
Ivan O <ioparin@pm.me>
2023-06-16 04:02:41 +0400

    updating to support local packages skipping

---
+++ Sources/xcgbootstrap/xcgen.swift
@@ -0,0 +1,193 @@
+import Foundation
+import ArgumentParser
+
+struct PackageList: Decodable {
+	var packages: [String: Remote]
+}
+
+struct PackageListPipeline: Decodable {
+	var packages: [String: RemoteTracked]
+}
+
+struct Remote: Decodable, Hashable {
+	let url: String?
+	let version: String?
+	let path: String?
+	
+	init(
+		url: String? = nil,
+		version: String? = nil,
+		path: String? = nil
+	) {
+		self.url = url
+		self.version = version
+		self.path = path
+	}
+}
+
+struct RemoteTracked: Decodable {
+	let url: String
+	let version: String
+	var checkoutAttempted: Bool
+}
+
+enum ParseError: Error {
+	case cannotDecodePackageList
+	case packageListHasntBeenDecoded
+}
+
+@main
+struct ClonePackagesCLI: ParsableCommand {
+	
+	static let configuration = CommandConfiguration(
+		abstract: "A brief description of my command-line tool",
+		subcommands: [
+			ClonePackagesRecursively.self
+		]
+	)
+}
+
+struct ClonePackagesRecursively: ParsableCommand {
+	static let configuration = CommandConfiguration(
+		commandName: "submodules",
+		abstract: "A brief description of submodules",
+		shouldDisplay: true
+	)
+	
+	static let repositoryBasePath = "Packages"
+	
+	@Argument(help: "Path to the json file containing information about packages")
+	var jsonFilePath: String
+	
+	func run() throws {
+		let fileManager = FileManager.default
+		let decoder = JSONDecoder()
+		let currentDirectoryURL = fileManager.currentDirectoryPath
+		let fileURL = URL(fileURLWithPath: currentDirectoryURL).appendingPathComponent(jsonFilePath)
+		let data = try Data(contentsOf: fileURL)
+		
+		var packageList: PackageList?
+		do {
+			packageList = try decoder.decode(PackageList.self, from: Data(contentsOf: fileURL))
+		} catch {
+			throw ParseError.cannotDecodePackageList
+		}
+		
+		guard let packageList else {
+			throw ParseError.packageListHasntBeenDecoded
+		}
+		
+		var remotePackages = packageList.packages
+			.filter {
+				$0.value.path == nil &&
+				$0.value.url != nil &&
+				$0.value.version != nil
+			}
+		
+		var checkedOutPackages: Set<Remote> = []
+		
+		while true {
+			var enriched = false
+			
+			for var (packageName, remote) in remotePackages {
+				guard !checkedOutPackages.contains(remote) else {
+					continue
+				}
+				print("\nProcessing package: \(packageName)")
+				
+				let repositoryName = remote.url!.split(separator: "/").last!
+				let folderName = repositoryName.replacingOccurrences(of: ".git", with: "")
+
+				let repositorySupposedURL = URL(fileURLWithPath: fileManager.currentDirectoryPath).deletingLastPathComponent()
+				let repositoryPath = repositorySupposedURL.absoluteString.appending("/\(Self.repositoryBasePath)/\(folderName)")
+				let repositorySupposedPath = fileManager.currentDirectoryPath.appending("/\(Self.repositoryBasePath)/\(folderName)")
+				if !fileManager.fileExists(atPath: repositorySupposedPath) {
+					let cloneTask = Process()
+					cloneTask.launchPath = "/usr/bin/env"
+					cloneTask.arguments = ["git", "clone", remote.url!, "\(Self.repositoryBasePath)/\(folderName)"]
+					cloneTask.standardOutput = FileHandle.nullDevice
+					cloneTask.standardError = FileHandle.nullDevice
+					cloneTask.launch()
+					cloneTask.waitUntilExit()
+				}
+				
+				let checkoutTask = Process()
+				checkoutTask.launchPath = "/usr/bin/env"
+				checkoutTask.arguments = ["git", "checkout", remote.version!]
+				checkoutTask.currentDirectoryPath = "\(Self.repositoryBasePath)/\(folderName)"
+				checkoutTask.standardOutput = FileHandle.nullDevice
+				checkoutTask.standardError = FileHandle.nullDevice
+				checkoutTask.launch()
+				checkoutTask.waitUntilExit()
+				if checkoutTask.terminationStatus == 0 {
+					print("Tag: \(remote.version!)")
+				} else {
+					print("❗️Could not checkout \(packageName), tag \(remote.version!). Check version tag and try again.")
+				}
+				
+				
+				
+				let packageResolveTask = Process()
+				packageResolveTask.launchPath = "/usr/bin/env"
+				packageResolveTask.arguments = ["swift", "package", "resolve"]
+				packageResolveTask.currentDirectoryPath = "\(Self.repositoryBasePath)/\(folderName)"
+				packageResolveTask.standardOutput = FileHandle.nullDevice
+				packageResolveTask.standardError = FileHandle.nullDevice
+				packageResolveTask.launch()
+				packageResolveTask.waitUntilExit()
+				
+				checkedOutPackages.insert(remote)
+				
+				let currentDirectoryURL = fileManager.currentDirectoryPath
+				let fileURL = URL(fileURLWithPath: currentDirectoryURL)
+					.appendingPathComponent(Self.repositoryBasePath)
+					.appendingPathComponent("\(folderName)")
+					.appendingPathComponent("Package.resolved")
+				
+				guard let pinsSerialized = try? Data(contentsOf: fileURL) else {
+					continue
+				}
+				
+				let packagePins = try decoder.decode(Pins.self, from: pinsSerialized)
+				let dependencies = extractDependencies(from: packagePins)
+				if !dependencies.isEmpty {
+					enriched = true
+					for dependency in dependencies {
+						let package = Remote(url: dependency.url, version: dependency.version)
+						remotePackages[dependency.name] = package
+					}
+				}
+			}
+			
+			if !enriched {
+				break
+			}
+		}
+	}
+	
+}
+
+
+func extractDependencies(from packageResolved: Pins) -> [Dependency] {
+	packageResolved.pins.map {
+		// rethink approach, we have revisions in pins, which are more secure // 🟡
+		Dependency(name: $0.identity, url: $0.location, version: $0.state.version)
+	}
+}
+
+struct Dependency {
+	var name: String
+	var url: String
+	var version: String
+}
+
+
+
+extension String {
+	func firstIndex(of substring: String) -> String.Index? {
+		if let range = self.range(of: substring) {
+			return range.lowerBound
+		}
+		return nil
+	}
+}
